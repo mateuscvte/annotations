1bd226d44bd184a9c37e0cc9508aa9567b99fbb8
Mateus Cavalcante <suporte19@rcostaconsult.com.br>
2020-03-29 01:55:16 -0300

    att project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDJC9QuEsBNWzsLsjGhewN2hr70Mim0Rpc",
-    authDomain: "karlanail.firebaseapp.com",
-    databaseURL: "https://karlanail.firebaseio.com",
-    projectId: "karlanail",
-    storageBucket: "karlanail.appspot.com",
-    messagingSenderId: "400135581880",
-    appId: "1:400135581880:web:b7d2c2826047458d27444d"
+    apiKey: "...",
+    authDomain: "...",
+    databaseURL: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "...",
+    appId: "..."
   }
 };
 
